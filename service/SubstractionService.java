11e6fe29fb2cbee094e55d1ae83808322f4ceb4e
Ivan Mysovskii <vano641@mail.ru>
2024-12-16 18:00:10 +0300

    Добавлено логирование

--- service/SubstractionService.java
+++ service/SubstractionService.java
@@ -4,11 +4,15 @@ import model.ComplexNumber;
 
 public class SubstractionService implements calculate{
 
+    private Loggable logger = new LoggerConsole();
+
     @Override
     public ComplexNumber getCalculate(ComplexNumber z1, ComplexNumber z2) {
         ComplexNumber result = new ComplexNumber();
+        logger.log("Выбрана операция Вычитания: z1 - z2 = " + "(" + z1 + ")" + " - " + "(" + z2 + ")" + "= ...");
         result.real = z1.real - z2.real;
         result.imagenary = z1.imagenary - z2.imagenary;
+        logger.log("Операция выполнена успешно, результат: ");
         return result;
     } 
 }
