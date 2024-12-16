f49af75c6001fcb21172303a3a1613f9a6ac410b
Ivan Mysovskii <vano641@mail.ru>
2024-12-16 15:59:35 +0300

    Реализован интерфейс calculate по методу Сложения

---
+++ service/SumService.java
@@ -0,0 +1,14 @@
+package service;
+
+import model.ComplexNumber;
+
+public class SumService implements calculate{
+
+    @Override
+    public ComplexNumber getCalculate(ComplexNumber z1, ComplexNumber z2) {
+        ComplexNumber result = new ComplexNumber();
+        result.real = z1.real + z2.real;
+        result.imagenary = z1.imagenary + z2.imagenary;
+        return result;
+        } 
+}
